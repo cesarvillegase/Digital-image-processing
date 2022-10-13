dee89ccb68bc118540df3d1a0e336c7a5749d30b
Cesar Alejandro Villegas Espindola <alex203020@icloud.com>
2022-10-13 12:38:57 -0500

    1st class DIP

--- DIP/1st.py
+++ DIP/1st.py
@@ -1,5 +1,76 @@
-import numpy as np
-import scipy as sp
-import matplotlib as plt
-import skimage
-import PIL
+#Creacion en escala de grises
+#Elaborado por César Alejandro Villegas Espíndola
+
+#Importar Librerias
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt #Se encarga de hacer gráficos
+import matplotlib.image as mpimg    
+import numpy as np                   #Se encarga de matrices
+
+M1 = [[1, 30, 45, 60, 100, 150, 190, 245],
+      [1, 30, 45, 60, 100, 150, 190, 245],
+      [1, 30, 45, 60, 100, 150, 90, 245],
+      [1, 30, 45, 60, 100, 150, 190, 245],
+      [1, 30, 45, 60, 100, 150, 190, 245],
+      [1, 30, 45, 60, 100, 150, 190, 245],
+      [1, 30, 45, 60, 100, 150, 190, 245],
+      [1, 30, 45, 60, 100, 150, 190, 245]]
+
+imgplot = plt.imshow(M1, cmap='gray')
+
+#Mostrar la imagen
+plt.show()
+print(M1)
+
+#Imagen en escala de grises pero con ciclo For
+#Crea una matriz de 255x255 con valores en cero en cada entrada
+M2 = np.zeros([255,255])
+#Control con for anidado de las filas y columnas
+for i in range(0,255):
+    for j in range(0,255):
+        M2[i,j] = j
+        
+imgplot = plt.imshow(M2, cmap='gray')
+plt.show()
+print(M2)
+
+#Crea una matriz de 255x255 con valores en cero en cada entrada
+M3 = np.zeros([256,256])
+#control con for anidado de las filas y columnas
+for i in range(0,256):
+    for j in range(0,256):
+        M3[i,j] = 256-j
+
+imgplot = plt.imshow(M3, cmap='gray')
+plt.show()
+print(M3)
+
+#Crea una matriz de 2000x1500 con valores en cero en cada entrada
+M4 = np.zeros([2000,1500])
+#control con for anidado de las filas y columnas
+for i in range(0,2000):
+    for j in range(0,1500):
+        M4[i,j] = 2000-j
+
+imgplot = plt.imshow(M4, cmap='gray')
+plt.show()
+
+#Crea una imagen vertical
+M5 = np.zeros([1500,1500])
+#control con for anidado de las filas y columnas
+for i in range(0,1500):
+    for j in range(0,1500):
+        if i==j:
+            M5[i,j]=0
+        elif abs(i-j) < 31:
+            M5[i,j]=0
+        
+        else:
+            M5[i,j]=255
+
+imgplot = plt.imshow(M5, cmap='gray')
+plt.show()
+print(M5)
+
+#Tarea hacer un asterísco
