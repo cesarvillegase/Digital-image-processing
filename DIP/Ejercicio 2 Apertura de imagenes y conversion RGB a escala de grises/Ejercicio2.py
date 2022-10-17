991947d5e3876c50f6adddf219febc40e6df3472
Cesar Alejandro Villegas Espindola <alex203020@icloud.com>
2022-10-17 08:06:01 -0500

    DIP

---
+++ DIP/Ejercicio 2 Apertura de imagenes y conversion RGB a escala de grises/Ejercicio2.py
@@ -0,0 +1,8 @@
+#Apertura de imagenes y conversión RGB a escala de grises
+#Elaborado por César Alejandro Villegas Espíndola
+
+#Importación de librerias 
+from matplotlib import pyplot as plt
+import matplotlib.image as mpimg
+
+img = mpimg.imread('cat.jpeg')
