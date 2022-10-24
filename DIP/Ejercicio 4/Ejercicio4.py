07b903d27af959681db132b0d95bc4853b8c0f86
Cesar Alejandro Villegas Espindola <alex203020@icloud.com>
2022-10-24 08:19:27 -0500

    DIP

--- DIP/Ejercicio 4/Ejercicio4.py
+++ DIP/Ejercicio 4/Ejercicio4.py
@@ -1,10 +1,11 @@
 # Histograma mediante librería PIL
 #Elaborado por César Alejandro Villegas Espíndola
 
-from matplotlib import pyplot as plt
-
 # Se importo desde PIL imagen
 from PIL import Image
+from matplotlib import pyplot as plt #De igual manera se puede importar como from matplotlib.pyplot as plt
+import numpy as np
+
 img = Image.open('/Users/cvillegas/workspace/Py/DIP/Ejercicio 4/Cat4.jpg')
 img_gris = img.convert('L')
 hist=img_gris.histogram()
@@ -13,3 +14,7 @@ plt.plot(hist, color='gray')
 plt.xlabel('Intensiddad')
 plt.ylabel('Cantidad de pixeles')
 plt.show()
+
+img = Image.open('/Users/cvillegas/workspace/Py/DIP/Ejercicio 4/Cat4.jpg')
+histograma=np.zeros(256)
+print(histograma)
