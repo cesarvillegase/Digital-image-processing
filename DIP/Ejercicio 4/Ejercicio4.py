331db45b1f82d82134cdca3d9f28b6a4aaccfcb8
Cesar Alejandro Villegas Espindola <alex203020@icloud.com>
2022-10-24 08:42:15 -0500

    DIP

--- DIP/Ejercicio 4/Ejercicio4.py
+++ DIP/Ejercicio 4/Ejercicio4.py
@@ -6,6 +6,7 @@ from PIL import Image
 from matplotlib import pyplot as plt #De igual manera se puede importar como from matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.image as mpimg
+from sklearn.metrics import homogeneity_completeness_v_measure
 
 img = Image.open('/Users/cvillegas/workspace/Py/DIP/Ejercicio 4/Cat4.jpg')
 img_gris = img.convert('L')
@@ -20,10 +21,13 @@ plt.show()
 #histograma=np.zeros(256)
 #print(histograma)
 
-R = img[:,:,0]
-G = img[:,:,1]
-B = img[:,:,2]
-
+#Dimensiones de la imagen 2048 × 2048
 img = mpimg.imread('/Users/cvillegas/workspace/Py/DIP/Ejercicio 4/Cat4.jpg')
-img_gris = 0.2989*R + 0.5870*G + 0.1140*B
+img_gris = 0.2989*img[:,:,0] + 0.5870*img[:,:,1] + 0.1140*img[:,:,2]
+
 histograma=np.zeros(256)
+print(img_gris[2, 2, 0]) # Prueba de acceso al arreglo
+for i in range(2049):
+    for j in range(2049):
+        histograma[img_gris[i,j]] = histograma[img_gris[i,j]] + 1
+print(histograma)
