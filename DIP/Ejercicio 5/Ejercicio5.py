a20963207b3ae918274b2a6482233c8ff3f4b599
Cesar Alejandro Villegas Espindola <alex203020@icloud.com>
2022-10-31 08:31:37 -0600

    DIP p

--- DIP/Ejercicio 5/Ejercicio5.py
+++ DIP/Ejercicio 5/Ejercicio5.py
@@ -42,3 +42,34 @@ print(imagen.shape[2])
 # Información a través de numpy, apertura es con PIL
 from numpy import asarray   #De ser necesario volver a importar PIL
 #from PIL import Image
+imagen2 = Image.open('/Users/cvillegas/workspace/Py/DIP/Ejercicio 5/car.jpeg')
+#Numpy trabaja con arreglos
+
+#Convertir la imagen en una variable de tipo "array"
+data = asarray(imagen2)
+print(data.shape)
+
+#Convertir un arreglo a una imagen
+imagen3 = Image.fromarray(data)
+print(imagen3.format)
+#Imprime el espacio de cpñpr 
+print(imagen3.mode)
+#Obtener las dimensiones de la imagen
+print(imagen3.size)
+
+##########################################################################
+#Manipular imágenes a traves de
+#PIL
+
+#from PIL import Image
+#Imagen3 = Image.fromarray(data)
+imagen3.thumbnail((100,50)) #El segundo número después de la coma es el porcentaje que se mantiene
+#El ratio de una imagen es igual al alto/ancho.
+print(imagen3.size)
+imagen3.show()
+
+#Rescalamiento de imagenes 2
+imagen4 = Image.open('/Users/cvillegas/workspace/Py/DIP/Ejercicio 5/car.jpeg')
+imagen_reescalada = imagen4.resize((100,50))
+print(imagen_reescalada.size)
+imagen_reescalada.show()
