d8d4717b933c3eeab202d652d66384d34654b743
Cesar Alejandro Villegas Espindola <alex203020@icloud.com>
2022-11-29 13:14:54 -0600

    Filtro digital

--- DIP/Ejercicio 8/Ejercicio8.py
+++ DIP/Ejercicio 8/Ejercicio8.py
@@ -1,8 +1,8 @@
-#Ejercicio 8 - Filtros digitales
-#Elaborado por César Alejandro Villegas Espíndola
-#Fecha: Lunes 28 de noviembre del 2022
+# Ejercicio 8 - Filtros digitales
+# Elaborado por César Alejandro Villegas Espíndola
+# Fecha: Lunes 28 de noviembre del 2022
 
-#Importación de librerias
+# Importación de librerias
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 from PIL import Image
@@ -12,9 +12,21 @@ img = mpimg.imread('/Users/cvillegas/workspace/Py/DIP/Ejercicio 8/saltandpeppern
 
 print(img)
 
-plt.imshow(img)
+# Se separan los espacios de color
+
+R=img[:,:,0]
+G=img[:,:,1]
+B=img[:,:,2]
+
+img_gris = 0.2989*R+0.5810*G+0.1140*B
+
+# Se muestra la imagen en escala de grises
+
+plt.imshow(img_gris, cmap='gray')
 plt.title("Imagen con ruido de sal y pimienta")
 plt.show()
+
+# Se imprime la forma de la imagen
 
 print(img.shape)
 
@@ -42,7 +54,7 @@ plt.imshow(img_filtrada)
 plt.title("Media")
 plt.show()
 
-#MEDIANA
+# MEDIANA
 
 img_filtrada1 = img.copy()
 
@@ -50,13 +62,38 @@ Imagen_ft1 = Image.fromarray(img_filtrada1)
 
 for i in range(ancho-1):
     for j in range(largo-1):
+        temp = [img[i-1, j-1], img[i, j-1], img[i+1, j-1],img[i-1, j], img[i, j], img[i+1, j], img[i-1, j+1], img[i-1, j+1], img[i+1, j+1]]
+        temp=sorted(temp)
+        Imagen_ft1[i,j] = temp[4]
+            
+plt.imshow(Imagen_ft1)
+plt.title("Mediana")
+plt.show()
+
+# MINIMO
+
+for i in range(1,largo-1):
+    for j in range(1,ancho-1):
         temp = [img[i-1, j-1], img[i, j-1], img[i+1, j-1],
         img[i-1, j], img[i, j], img[i+1, j], 
         img[i-1, j+1], img[i-1, j+1], img[i+1, j+1]]
-        temp=sorted(temp)
-        Imagen_ft1[i,j] = temp[4]
-        
-        
+        temp=min(temp)
+        Imagen_ft1[i,j] = temp
+
 plt.imshow(Imagen_ft1)
-plt.title("Mediana")
+plt.title("Minimo")
+plt.show()
+
+# MAXIMO
+
+for i in range(1,largo-1):
+    for j in range(1,ancho-1):
+        temp = [img[i-1, j-1], img[i, j-1], img[i+1, j-1],
+        img[i-1, j], img[i, j], img[i+1, j], 
+        img[i-1, j+1], img[i-1, j+1], img[i+1, j+1]]
+        temp=max(temp)
+        Imagen_ft1[i,j] = temp
+
+plt.imshow(Imagen_ft1)
+plt.title("Maximo")
 plt.show()
